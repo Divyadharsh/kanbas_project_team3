a654e2a8ee0d2025fd9d4242341b6e9d67cdd710
mchen2008 <35387850+lusystemsbio@users.noreply.github.com>
2024-08-18 22:27:23 -0400

    landing page

--- src/LandingPage/index.tsx
+++ src/LandingPage/index.tsx
@@ -1,5 +1,6 @@
 
 //import { Route, Routes, Navigate } from "react-router";
+import { FaHryvnia } from "react-icons/fa";
 import TOC from "./TOC";
 //import { Provider } from "react-redux";
 
@@ -10,9 +11,44 @@ export default function LandingPage() {
 
         <div id="wd-labs" className="container mt-2">
             <h1>CS5610 Web Development</h1>
-            <h2>Team Project</h2>
-            <h3>Team Member: Divya, Kaumudi, Lindsey, Thomas, Man Chen</h3>
-            <TOC />
+            <hr />
+            <h2>Team Project: develop a Quizzes section </h2>
+
+            <br />
+
+            <ul className="nav nav-pills">
+                <li className="nav-item">
+                    <a id="wd-k" href="#/Kanbas" className="nav-link">
+                        <h4>Kanbas</h4>
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a id="wd-k" href="https://github.com/Divyadharsh/kanbas_project_team3_frontend.git" className="nav-link">
+                        <h4> Frontend GitHub </h4>
+                    </a>
+                </li>
+
+                <li className="nav-item">
+                    <a id="wd-k" href="https://github.com/Divyadharsh/kanbas_project_team3_backend.git" className="nav-link">
+                        <h4> Backend GitHub </h4>
+                    </a>
+                </li>
+
+            </ul>
+            <br />
+            <h3>Team Member:
+                <br />
+
+                <ul className="list-group border-0">
+                    <li className="list-group-item border-0 ">Divyadharshini Muruganandham </li>
+                    <li className="list-group-item border-0">Kaumudi Rajesh Rawal</li>
+                    <li className="list-group-item border-0">Lindsey Sutermeister</li>
+                    <li className="list-group-item border-0">Thomas Simmons</li>
+                    <li className="list-group-item border-0">Man Chen</li>
+
+                </ul>
+            </h3>
+
 
         </div>
 
